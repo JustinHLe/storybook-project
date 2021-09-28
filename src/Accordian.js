20466cd35a81f5b3e803fb4512202ba3d65c1d83
Le <jl17813@m38760m1.appliedmed.com>
2021-09-28 13:43:40 -0700

    finish accordian

--- src/Accordian.js
+++ src/Accordian.js
@@ -2,16 +2,22 @@ import styles from './styles/accordian.module.css'
 import PropTypes, { array } from 'prop-types'
 import { useState } from 'react'
 export const Accordian = ({handleClick, action, arrayText}) => {
-    function showContent(e){
+    function showContent(e,i){
         e.stopPropagation()
         if(e.target.className === 'accordian-module__box__1Qx9E'){
             handleClick()
             return;
         }
+        const rowToggle = e.target
         const arrowToggle = e.target.firstChild.firstChild
         const contentToggle = e.target.nextSibling
         arrowToggle.classList.toggle(styles.arrow_dropdown)
         contentToggle.classList.toggle(styles.shown)
+        if(i === arrayText.length - 1){
+            contentToggle.classList.toggle(styles.shown_no_border)
+            return;
+        }
+        rowToggle.classList.toggle(styles.row_no_border)
     }
     return(
         <body className={styles.wrapper}>
@@ -19,7 +25,7 @@ export const Accordian = ({handleClick, action, arrayText}) => {
                 {arrayText.map(({title,content},i)=>{
                     return(
                         <>
-                        <div className={styles.row} id={i} key = {i} onClickCapture={(e)=>{showContent(e,i)}}>
+                        <div className={i === arrayText.length - 1 ? styles.row_no_border : styles.row} id={i} key = {i} onClickCapture={(e)=>{showContent(e,i)}}>
                             <div className={styles.info_container}>
                                 <div className={styles.arrow} id = "test" style={{pointerEvents: 'none'}} style={{pointerEvents: 'none'}}></div>
                                 <span style={{pointerEvents: 'none'}}>{title}</span>
