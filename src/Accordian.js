da4b087d1bfea815a152b48b773e399169e29616
Le <jl17813@m38760m1.appliedmed.com>
2021-09-29 09:28:04 -0700

    completed accordian

--- src/Accordian.js
+++ src/Accordian.js
@@ -1,23 +1,63 @@
 import styles from './styles/accordian.module.css'
 import PropTypes, { array } from 'prop-types'
 import { useState } from 'react'
-export const Accordian = ({handleClick, action, arrayText}) => {
+export const Accordian = ({handleClick, action, arrayText, nested, innerArrayText}) => {
     function showContent(e,i){
         e.stopPropagation()
         if(e.target.className === 'accordian-module__box__1Qx9E'){
             handleClick()
             return;
         }
-        const rowToggle = e.target
         const arrowToggle = e.target.firstChild.firstChild
-        const contentToggle = e.target.nextSibling
+        const contentToggle = e.target.lastChild
         arrowToggle.classList.toggle(styles.arrow_dropdown)
-        contentToggle.classList.toggle(styles.shown)
-        if(i === arrayText.length - 1){
-            contentToggle.classList.toggle(styles.shown_no_border)
+        if(e.target.id.includes('inner')){
+            contentToggle.classList.toggle(styles.shown_inner)
             return;
         }
-        rowToggle.classList.toggle(styles.row_no_border)
+        contentToggle.classList.toggle(styles.shown)
+    }
+    if(nested){
+        return (
+            <body className={styles.wrapper}>
+            <div className={styles.accordian_wrapper}> 
+                {arrayText.map(({title,content},i)=>{
+                    return(
+                        <>
+                        <div className={styles.row} id={`outer ${i}`} key = {i} onClickCapture={(e)=>{showContent(e,i)}}>
+                            <div className={styles.info_container}>
+                                <div className={styles.arrow} id = "test" style={{pointerEvents: 'none'}} style={{pointerEvents: 'none'}}></div>
+                                <div style={{pointerEvents: 'none', overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis", display: 'block', width: '43vw'}}>{title}</div>
+                                <div className={action ? styles.box : null} onClick={handleClick}>
+                                    <div className={action ? styles.triangle : null}></div>
+                                </div>
+                            </div>
+                            <div className={styles.hidden}>
+                                {innerArrayText.map(({title, content, row},itr)=>{
+                                    if(i === row){
+                                        return(
+                                            <>
+                                            <div className={styles.inner_row} id = {`inner ${i}`} key={itr} onClickCapture={(e)=>{showContent(e,i)}}>
+                                                <div className={styles.info_container}>
+                                                    <div className={styles.arrow} id = "test" style={{pointerEvents: 'none'}} style={{pointerEvents: 'none'}}></div>
+                                                    <div style={{pointerEvents: 'none', overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis", display: 'block', width: '40vw'}}>{title}</div>
+                                                </div>
+                                                <div className={styles.hidden}>
+                                                    <p>{content}</p>
+                                                </div>
+                                            </div>
+                                            </>
+                                        )
+                                    }
+                                })}
+                            </div>
+                        </div>  
+                        </>
+                    )
+                })}
+            </div>
+        </body>   
+        )
     }
     return(
         <body className={styles.wrapper}>
@@ -25,18 +65,18 @@ export const Accordian = ({handleClick, action, arrayText}) => {
                 {arrayText.map(({title,content},i)=>{
                     return(
                         <>
-                        <div className={i === arrayText.length - 1 ? styles.row_no_border : styles.row} id={i} key = {i} onClickCapture={(e)=>{showContent(e,i)}}>
+                        <div className={styles.row} id={`outer ${i}`} key = {i} onClickCapture={(e)=>{showContent(e,i)}}>
                             <div className={styles.info_container}>
                                 <div className={styles.arrow} id = "test" style={{pointerEvents: 'none'}} style={{pointerEvents: 'none'}}></div>
-                                <span style={{pointerEvents: 'none'}}>{title}</span>
-                            </div>
-                            <div className={action ? styles.box : null} onClick={handleClick}>
+                                <div style={{pointerEvents: 'none', overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis", display: 'block', width: '43vw'}}>{title}</div>
+                                <div className={action ? styles.box : null} onClick={handleClick}>
                                     <div className={action ? styles.triangle : null}></div>
+                                </div>
+                            </div>
+                            <div className={styles.hidden}>
+                                <p>{content}</p>
                             </div>
                         </div>  
-                        <div className={styles.hidden}>
-                                <p>{content}</p>
-                        </div>
                         </>
                     )
                 })}
--- src/stories/Accordian.stories.js
+++ src/stories/Accordian.stories.js
@@ -11,10 +11,11 @@ const Template = args => <Accordian {...args} />
 export const Default = Template.bind({})
 Default.args=({
     action: false,
+    nested: false,
     arrayText: [
     {
-        title : "headerOne", 
-        content : "hi"
+        title : "Veniam tempor dolore elit Lorem. Dolore ipsum id eiusmod elit. Nisi proident labore nostrud dolor deserunt elit tempor mollit. Enim esse ad ipsum fugiat reprehenderit excepteur sit. Est ex mollit amet esse sunt incididunt excepteur commodo excepteur ad aliqua consequat irure consectetur. Excepteur deserunt officia nostrud aute ut minim nisi mollit anim officia adipisicing in. Quis aliquip consequat adipisicing officia excepteur duis do aliqua et excepteur magna consectetur laborum nostrud.", 
+        content : "Ut aute magna consectetur voluptate deserunt eu amet nisi officia esse tempor esse sint. Elit eu eiusmod Lorem duis nostrud aliqua culpa velit. Elit enim velit cupidatat eiusmod enim dolor consectetur nisi."
     }, 
     {
         title : "headerTwo",
@@ -25,4 +26,32 @@ Default.args=({
         content : "cya"
     }
 ]
+})
+
+export const Nested = Template.bind({})
+Nested.args=({
+    ...Default.args,
+    nested: true,
+    innerArrayText: [
+        {
+            row: 0,
+            title : "Inner header one", 
+            content : "hi"
+        }, 
+        {
+            row: 0,
+            title : "ANOTHER ONE", 
+            content : "DJ KHALED"
+        }, 
+        {
+            row: 1,
+            title : "Ea ea esse mollit quis amet sit dolore ullamco laboris aliqua magna excepteur. Eiusmod cillum aliqua ad culpa minim deserunt occaecat voluptate. Excepteur in non nulla in reprehenderit anim pariatur ad. Labore dolore deserunt deserunt adipisicing labore. Deserunt duis quis aute aute irure.",
+            content : "Laborum irure enim mollit aute labore eiusmod ea cupidatat sint nulla laboris. Cillum mollit eiusmod esse dolor dolor eiusmod. Lorem labore nisi culpa incididunt sit adipisicing ex nulla elit cillum. Tempor non excepteur adipisicing sunt magna."
+        },
+        {
+            row: 2,
+            title : "Inner headerThree",
+            content : "cya"
+        } 
+    ]
 })
